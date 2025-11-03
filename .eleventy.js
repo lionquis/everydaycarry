6f0365b8bdcd55aa1fe642afa7c0a2529d305740
lionquis <lionquis@msu.edu>
2025-11-03 04:17:06 +0000

    site

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function(eleventyConfig) {
         let metadata = await Image(src, {
           widths: [300, 600, 1000],
           formats: ["png"],
-          outputDir: "./_site/img/",
+          outputDir: "./_site/images/",
           urlPath: "/img/"
         });
 
