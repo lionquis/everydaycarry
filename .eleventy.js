293305fe457eaab2be7521d1ffa02d801374dc56
lionquis <lionquis@msu.edu>
2025-11-03 04:42:40 +0000

    shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -8,22 +8,14 @@ module.exports = function(eleventyConfig) {
   const Image = require("@11ty/eleventy-img");
 
     module.exports = function(eleventyConfig) {
-      eleventyConfig.addShortcode("image", async function(src, alt, sizes = "100vw") {
+      eleventyConfig.addLiquidShortcode("image", async function(src, alt = "", sizes = "100vw") {
         let metadata = await Image(src, {
-          widths: [300, 600, 1000],
-          formats: ["png"],
-          outputDir: "/_site/images/",
+          widths: [300, 600, 1200],
+          formats: ["png", "webp", "jpeg"],
+          outputDir: "./_site/img/",
           urlPath: "/img/"
         });
 
-        let imageAttributes = {
-          alt,
-          sizes,
-          loading: "lazy",
-          decoding: "async"
-        };
-
+        let imageAttributes = { alt, sizes, loading: "lazy", decoding: "async" };
         return Image.generateHTML(metadata, imageAttributes);
       });
-    };
-  };
