419fd9cd1a2241465ec8406b8bfa91654f0672f7
lionquis <lionquis@msu.edu>
2025-11-03 04:50:49 +0000

    a

--- .eleventy.js
+++ .eleventy.js
@@ -3,14 +3,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
-  eleventyConfig.addShortcode("readableDate", (date) => {
-  return new Date(date).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  });
-});
+  
 }
